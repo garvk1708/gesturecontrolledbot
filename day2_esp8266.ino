c07df49372300d790f24cdec640409ca7c030bad
Garv <68726571+garvk1708@users.noreply.github.com>
2024-10-21 23:46:21 +0530

    Update day2_esp8266.ino

--- day2_esp8266.ino
+++ day2_esp8266.ino
@@ -1,144 +1,119 @@
 #include <ESP8266WiFi.h>
-// Motor driver pin definitions
+
 #define IN1 D1  
 #define IN2 D2
 #define IN3 D3
 #define IN4 D4
-#define ENA D5  // PWM pin for Motor A
-#define ENB D6  // PWM pin for Motor B
+#define ENA D5
+#define ENB D6
 
-// Define PWM properties
-const int dutyCycle1 = 500;   // Speed duty cycle for forward/backward
-const int dutyCycle2 = 500;   // Speed duty cycle for turning
-
-/****************************************************/
-// WiFi credentials
-//Enter according to your device
+const int maxDutyCycle = 1001;  
+const int rampUpStep = 10;       
+const int rampUpDelay = 20;     
 
 const char* ssid = "MYWORLD07 1443";
 const char* password = "NAmi94@#";
 
-/****************************************************/
-
-// WiFi server on port 80
 WiFiServer server(80);
 
 void setup() {
-  // Start Serial communication
   Serial.begin(115200);
-  
-  // Set motor driver pins as outputs
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
-  pinMode(2, OUTPUT); //LED is connected to pin 2 for status indication
+  pinMode(2, OUTPUT);
 
-  // Configure PWM 
-  analogWriteRange(1023); // Set PWM range (0-1023 for ESP8266)
-
-  // Connect to Wi-Fi
+  analogWriteRange(1023);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
 
-  // Wi-Fi connected, start the server
   Serial.println("Connected to WiFi");
   server.begin();
-  Serial.println("Server started");
-
-  // Display the IP address
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // Check if a new client has connected
   WiFiClient client = server.available();
   if (client) {
     Serial.println("New client connected"); 
-
-    // While the client is connected, process incoming requests
     while (client.connected()) {
       if (client.available()) {
         String request = client.readStringUntil('\r');
         Serial.print("Received command: ");
         Serial.println(request);
 
-        // Process commands
         if (request.endsWith("one")) {
-          Serial.println("Command: Move Forward");
           moveForward();
         } else if (request.endsWith("two")) {
-          Serial.println("Command: Turn Right");
           turnRight();
         } else if (request.endsWith("three")) {
-          Serial.println("Command: Turn Left");
           turnLeft();
         } else if (request.endsWith("four")) {
-          Serial.println("Command: Move Backward");
           moveBackward();
         } else if (request.endsWith("zero")) {
-          Serial.println("Command: Stop");
           stopMoving();
         }
       }
     }
 
-    // Client disconnected
     client.stop();
-    digitalWrite(2, LOW);  // Turn off the LED to indicate the client disconnected
+    digitalWrite(2, LOW);
     Serial.println("Client disconnected");
   }
 }
 
-// Function to move the robot forward
+void rampUpPWM(int pin, int targetDutyCycle) {
+  for (int dutyCycle = 0; dutyCycle <= targetDutyCycle; dutyCycle += rampUpStep) {
+    analogWrite(pin, dutyCycle);
+    delay(rampUpDelay);
+  }
+}
+
 void moveForward() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, dutyCycle1); // Set speed for ENA 
-  analogWrite(ENB, dutyCycle1); // Set speed for ENB 
+  rampUpPWM(ENA, maxDutyCycle);
+  rampUpPWM(ENB, maxDutyCycle);
   Serial.println("Moving Forward");
 }
 
-// Function to move the robot backward
 void moveBackward() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
-  analogWrite(ENA, dutyCycle1); // Set speed for ENA 
-  analogWrite(ENB, dutyCycle1); // Set speed for ENB 
+  rampUpPWM(ENA, maxDutyCycle);
+  rampUpPWM(ENB, maxDutyCycle);
   Serial.println("Moving Backward");
 }
 
-// Function to turn the robot left
 void turnLeft() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, dutyCycle2); // Set lower speed for ENA 
-  analogWrite(ENB, dutyCycle2); // Set lower speed for ENB 
+  rampUpPWM(ENA, maxDutyCycle / 2);
+  rampUpPWM(ENB, maxDutyCycle / 2);
   Serial.println("Turning Left");
 }
 
-// Function to turn the robot right
 void turnRight() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
-  analogWrite(ENA, dutyCycle2); // Set lower speed for ENA 
-  analogWrite(ENB, dutyCycle2); // Set lower speed for ENB 
+  rampUpPWM(ENA, maxDutyCycle / 2);
+  rampUpPWM(ENB, maxDutyCycle / 2);
   Serial.println("Turning Right");
 }
 
-// Function to stop the robot
 void stopMoving() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
